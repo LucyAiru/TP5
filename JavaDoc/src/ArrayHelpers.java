92836690ce555319b67655ba500b9f4cc2ddb0e9
PedroKinetica <pedro@kinetica-solutions.com>
2017-09-12 23:58:03 -0300

    Estructura baica para el TP5

---
+++ JavaDoc/src/ArrayHelpers.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class ArrayHelpers {
+
+	//este método ordena los elementos de una lista menor a mayor usando el método de burbujeo.
+	public <T> ArrayList<T> OrdenarAscendente(ArrayList<T> lista) {
+		return lista;
+	}
+	
+	//este método ordena los elementos de una lista mayor a menor usando el método de burbujeo.
+	public <T> ArrayList<T> ordenarDescendente(ArrayList<T> lista) {
+		return lista;
+	}
+	
+	//a partir de dos listas este método genera una tercera con la mezcla de las dos.
+	public <T> ArrayList<T> mezcla(ArrayList<T> listaUno, ArrayList<T> listaDos) {
+		return listaUno;
+	}
+	
+	//a partir de dos listas, este método las ordena y devuelve una tercera con la mezcla de las dos manteniendo el orden.
+	public <T> ArrayList<T> mezclaOrdenada(ArrayList<T> listaUno, ArrayList<T> listaDos) {
+		return listaUno;
+	}
+	
+	//a partir de una lista, este método devuelve la misma lista en orden inverso al que tiene.
+	public <T> ArrayList<T> marchaAtras(ArrayList<T> lista) {
+		return lista;
+	}
+
+	//este método devuelve el elemento que más veces aparece en una lista.
+	public <T> T elementoMasRepetido(ArrayList<T> lista) {
+		return lista.get(0);
+	}
+	
+	//a partir de una lista, este método devuelve la lista sin elementos repetidos.
+	public <T> ArrayList<T> quitarRepetidos(ArrayList<T> lista) {
+		return lista;
+	}
+	
+}
